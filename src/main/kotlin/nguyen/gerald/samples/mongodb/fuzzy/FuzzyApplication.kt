b08bcfc52d08ab0d640a8fef918b9d160e58ce47
Gerald Nguyen <ngthehuy@gmail.com>
2023-01-07 16:57:27 +0800

    initial setup

---
+++ src/main/kotlin/nguyen/gerald/samples/mongodb/fuzzy/FuzzyApplication.kt
@@ -0,0 +1,11 @@
+package nguyen.gerald.samples.mongodb.fuzzy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FuzzyApplication
+
+fun main(args: Array<String>) {
+	runApplication<FuzzyApplication>(*args)
+}
---
+++ src/test/kotlin/nguyen/gerald/samples/mongodb/fuzzy/FuzzyApplicationTests.kt
@@ -0,0 +1,13 @@
+package nguyen.gerald.samples.mongodb.fuzzy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FuzzyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
