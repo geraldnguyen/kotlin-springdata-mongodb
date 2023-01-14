a9580d7e5133d7c8b5cac360ac120cf51d753804
Gerald Nguyen <ngthehuy@gmail.com>
2023-01-14 23:52:45 +0800

    flavour #1 with pagination

--- src/main/kotlin/nguyen/gerald/samples/mongodb/fuzzy/api/CommentController.kt
+++ src/main/kotlin/nguyen/gerald/samples/mongodb/fuzzy/api/CommentController.kt
@@ -3,8 +3,11 @@ package nguyen.gerald.samples.mongodb.fuzzy.api
 import nguyen.gerald.samples.mongodb.fuzzy.data.Comment
 import nguyen.gerald.samples.mongodb.fuzzy.data.CommentRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,7 +17,14 @@ class CommentController {
     private lateinit var commentRepository: CommentRepository
 
     @GetMapping(value = [ "", "/" ])
-    fun listAll(): List<Comment> {
-        return commentRepository.findAll()
+    fun listAll(
+        @RequestParam(value = "_size", required = false, defaultValue = "10") size: Int,
+        @RequestParam(value = "_page", required = false, defaultValue = "0") page: Int
+    ): List<Comment> {
+        val pageable = Pageable.ofSize(size).withPage(page);
+
+        val result: Page<Comment> = commentRepository.findAll(pageable)
+
+        return result.content
     }
 }
